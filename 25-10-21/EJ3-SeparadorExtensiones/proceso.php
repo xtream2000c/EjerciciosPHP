44e207e666a45d4d3b88a2f2bf4c0fbac36ac38f
xtream2000c <carlmarcast@gmail.com>
2021-10-26 16:01:03 +0200

    EJ3 Completado

--- 25-10-21/EJ3-SeparadorExtensiones/proceso.php
+++ 25-10-21/EJ3-SeparadorExtensiones/proceso.php
@@ -1,5 +1,45 @@
 <?php
 
-    
+    $nombre = $_POST['nombre'];
+
+    $palabras = explode('.',strtoupper($nombre));
+    $extension = $palabras['1'];
+
+    echo "a) <h1>".$extension."</h1>";
+
+    echo "b) El documento es de tipo: ";
+
+    switch($extension){
+        case 'PDF':
+            echo "<b> Documento Adobe PDF </b>" ;
+        break;
+        case 'TXT':
+            echo "<b> Documento De Texto </b>" ;
+        break;
+        case 'HTML':
+            echo "<b> Documento HTML </b>" ;
+        break;
+        case 'HTM':
+            echo "<b> Documento HTML </b>" ;
+        break;
+        case 'PPT':
+            echo "<b> Presentacion Microsoft Powerpoint </b>" ;
+        break;
+        case 'DOC':
+            echo "<b> Documento Microsoft Word </b>" ;
+        break;
+        case 'GIF':
+            echo "<b> Imagen Gif </b>" ;
+        break;
+        case 'PPT':
+            echo "<b> Imagen JPG </b>" ;
+        break;
+        case 'DOC':
+            echo "<b> Archivo comprimido ZIP </b>" ;
+        break;
+        default:
+            echo "Archivo ". $extension;
+        break;
+}
 
 ?>
