cc9ad12e6368bd70ee394cf5104bd98d368a2d0f
xtream2000c <carlmarcast@gmail.com>
2021-11-02 15:39:31 +0100

    Opcion 1 EJ10 Completada

---
+++ 25-10-21/EJ10-OperacionesConBits/op1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+    $cadena1 = "";
+
+    for ($i=0; $i < 10 ; $i++) { 
+        $cadena1 = $cadena1 ." ".rand(0,1);
+    }
+    
+    echo "Cadena inicial: ". $cadena1;
+
+    $cadena2 = "";
+
+    for ($i=0; $i < strlen($cadena1); $i++) { 
+        if ($cadena1[$i] == " "){
+            $cadena2 = $cadena2 . " ";
+        }else if($cadena1[$i] == 0){
+            $cadena2 = $cadena2 . 1;
+        }else{
+            $cadena2 = $cadena2 . 0;
+        }
+    }
+    echo "<br>";
+    echo "Cadena complementaria: ". $cadena2;
+
+?>
+</body>
+</html>
